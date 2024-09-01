10f5eb59ab75ea5c6930f1c3dcba8a5944ef2d2e
shirmtry <dinhtanhuy547@gmail.com>
2024-09-01 13:53:36 +0700

    STRONG

---
+++ SUPPORT.bat
@@ -0,0 +1,13 @@
+/tạo 1 file để làm cho tất cả các file kia có thể chạy theo 1 thức tự /
+
+@echo off
+call "C:\Path\To\Your\BatFile1.bat"
+call "C:\Path\To\Your\BatFile2.bat"
+call "C:\Path\To\Your\BatFile3.bat"
+call "C:\Path\To\Your\BatFile4.bat"
+call "C:\Path\To\Your\BatFile5.bat"
+call "C:\Path\To\Your\BatFile6.bat"
+call "C:\Path\To\Your\BatFile7.bat"
+call "C:\Path\To\Your\BatFile8.bat"
+call "C:\Path\To\Your\BatFile9.bat"
+call "C:\Path\To\Your\BatFile10.bat"
